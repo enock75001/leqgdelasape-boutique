09a5eee7bf79ce85538a6314909850c542c62c18
ENOCK DE FRANCKY <franckenock78@gmail.com>
2025-07-30 09:05:37 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  "projectId": "le-bleu-water-hub",
+  "appId": "1:342431785251:web:4e1b42a34b6d42f2416c96",
+  "storageBucket": "le-bleu-water-hub.firebasestorage.app",
+  "apiKey": "AIzaSyB9jCs2ZFTWb2p3B2fsgZ-8rPZT2HdbMuE",
+  "authDomain": "le-bleu-water-hub.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "342431785251"
 };
 
 // Initialize Firebase
